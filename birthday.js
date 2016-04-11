3940e338ef8ed604f401aee91e1c93ada53ead4c
Richard Mbouombouo <RichardMbouombouo@gmail.com>
2016-04-10 21:39:26 -0500

    removed a few redundencies

--- birthday.js
+++ birthday.js
@@ -1,7 +1,7 @@
 // Can you find after how many years will a person's birthday fall on the same day of the week that he was born?
 //
 // For example, Joy's birthday is on 16th October, 1990 which falls on Friday. After how many years will his birthday fall on Friday again? (That would be 11 years)
-
+//Nice
 //ms = 31622400 * 1000
 function next(year, month, date){
     var bDay = new Date(year + "-" + month + "-" + date);
